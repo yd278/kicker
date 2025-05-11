3485bc1d7175153438e77682a4a83a4287c0e4d3
Y. Ding <argentumdyz@gmail.com>
2025-05-10 17:34:58 -0700

    Pull components out

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <script setup lang="ts">
 import Selector from './components/Selector.vue';
-import { NButton , NIcon} from 'naive-ui';
-import { SettingsOutline } from '@vicons/ionicons5'
+import StartButton from './components/StartButton.vue';
 </script>
 
 <template>
   <Selector/>
   <br>
-  <n-button size="large" type="primary">🥾踢踢！</n-button>
-    <div class="settings-fab">
-    <n-button circle type="primary" size="large">
-      <n-icon>
-        <SettingsOutline />
-      </n-icon>
-    </n-button>
-  </div>
+  <StartButton/>
+
 </template>
 
 <style scoped>
-.settings-fab {
-  position: fixed;
-  right: 24px;
-  bottom: 24px;
-  z-index: 1000;
-}
 </style>
---
+++ src/components/StartButton.vue
@@ -0,0 +1,7 @@
+<script setup lang='ts'>
+import { NButton } from 'naive-ui';
+</script>
+
+<template>
+    <n-button size="large" type="primary">🥾踢踢！</n-button>
+</template>
